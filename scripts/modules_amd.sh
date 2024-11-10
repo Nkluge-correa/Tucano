7bcf040f0097d83342f05b96779dfba7256267f4
Nicholas <nicholas@airespucrs.org>
2024-11-10 23:51:51 +0100

    udated requirements.txt

---
+++ scripts/modules_amd.sh
@@ -0,0 +1,5 @@
+export MODULEPATH=/opt/software/easybuild-AMD/modules/all:/etc/modulefiles:/usr/share/modulefiles:/opt/software/modulefiles:/usr/share/modulefiles/Linux:/usr/share/modulefiles/Core:/usr/share/lmod/lmod/modulefiles/Core
+
+module purge
+
+module load UCX-CUDA/1.14.1-GCCcore-12.3.0-CUDA-12.1.1 Python/3.11.3-GCCcore-12.3.0
---
+++ scripts/modules_intel.sh
@@ -0,0 +1,3 @@
+module --force purge
+
+module load UCX-CUDA/1.14.1-GCCcore-12.3.0-CUDA-12.1.1 Python/3.11.3-GCCcore-12.3.0
